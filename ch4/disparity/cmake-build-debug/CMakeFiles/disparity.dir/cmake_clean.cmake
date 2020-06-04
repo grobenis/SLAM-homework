e03d48bd31d63f55ae213d27c72998eb48b18629
grobenis <guob1996@outlook.com>
2020-06-05 01:52:06 +0800

    1

---
+++ ch4/disparity/cmake-build-debug/CMakeFiles/disparity.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/disparity"
+  "../bin/disparity.pdb"
+  "CMakeFiles/disparity.dir/disparity.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/disparity.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
