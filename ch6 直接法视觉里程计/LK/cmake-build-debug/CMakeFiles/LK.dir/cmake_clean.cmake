ce9f1ba2e5e3b60d1c9fa15fc011e0b9582ad87b
grobenis <guob1996@outlook.com>
2020-06-26 12:09:11 +0800

    ch6

---
+++ ch6 直接法视觉里程计/LK/cmake-build-debug/CMakeFiles/LK.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/LK"
+  "../bin/LK.pdb"
+  "CMakeFiles/LK.dir/optical_flow.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
